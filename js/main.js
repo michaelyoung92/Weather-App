cd2505b5dae225694acaa510f2e60b49f6cf2f34
Michael Young <michaelbyoung9208@gmail.com>
2021-03-03 20:36:27 +0000

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,68 +1,3 @@
-// const api = {
-//     key: '39b44eaa66c8464bda8ff71fc2a80638',
-//     base: 'https://api.openweathermap.org/data/2.5/'
-// }
-
-// const main = document.querySelector('main');
-// main.style.display = 'none';
-
-// const searchBox = document.querySelector('.search-box');
-// searchBox.addEventListener('keypress', setQuery);
-
-// function setQuery(evt) {
-//     if(evt.keyCode == 13) {
-//         getResults(searchBox.value);
-//     }
-// }
-
-// function getResults(query) {
-//     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-//     .then(weather => {
-//         return weather.json();
-//     }).then(displayResults);
-// }
-
-
-// function displayResults(weather) {
-//     console.log(weather);
-//     let city = document.querySelector('.location .city');
-//     city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-//     let now = new Date();
-//     let date = document.querySelector('.location .date');
-//     date.innerText = dateBuilder(now);
-
-//     let temp = document.querySelector('.current .temp');
-//     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-
-//     let weather_el = document.querySelector('.current .weather');
-//     weather_el.innerText = weather.weather[0].main;
-
-//     let hilow = document.querySelector('.hi-low');
-//     hilow.innerText  =`${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-
-//     main.style.display = 'block';
-// }
-
-// function dateBuilder(d) {
-//     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-//     const days = ["Sun","Mon","Tues","Wed","thurs","Fri","Sat"];
-
-//     let day = days[d.getDay()];
-//     let date = d.getDate();
-//     let month = months[d.getMonth()];
-//     let year = d.getFullYear();
-
-//     return `${day} ${date} ${month} ${year}`;
-// }
-
-
-
-
-
-
-
 //Long Range
 
 const longApi = {
@@ -89,6 +24,7 @@ function getResults(query) {
 
 function displayResults(weather) {
     console.log(weather);
+    const weatherTemp = weather.list[0].main.temp;
 
     let city = document.querySelector('.location');
     city.innerText = `${weather.city.name}, ${weather.city.country}`;
@@ -98,7 +34,7 @@ function displayResults(weather) {
     date.innerText = dateBuilder(now);
 
     let temp = document.querySelector('.temperature');
-    temp.innerHTML = `${Math.round(weather.list[0].main.temp)}<span>°c</span>`;
+    temp.innerHTML = `${Math.round(weatherTemp)}<span>°c</span>`;
 
     let weatherImg = document.querySelector('.weather-img');
 
@@ -120,6 +56,11 @@ function displayResults(weather) {
             weatherImg.src = `img/svg/${icon}.svg`;
         }
     });
+
+    //Weather Description
+    const weatherDescription = document.querySelector('.weather-description');
+    weatherDescription.innerText = weather.list[0].weather[0].description;
+
 
     //Min & Max Temps
     let minMax = document.querySelector('.min-max');
@@ -144,22 +85,55 @@ function displayResults(weather) {
 
     //Background Color Changer
     const body = document.querySelector('body');
-    const colorList = [
-        {weather: 'Clear', color1: '#7AE7C7', color2: '#72C1E1'},
-        {weather: 'Clouds', color1: '#F981BB', color2: '#7F7E84'},
-        {weather: 'Drizzle', color1: '#b2c9c8', color2: '#698b8b'},
-        {weather: 'Fog', color1: '#C5B2A6', color2: '#7F7E84'},
-        {weather: 'Rain', color1: '#504AC4', color2: '#59AED1'},
-        {weather: 'Snow', color1: '#bfc9cf', color2: '#77BDE0'},
-        {weather: 'Thunderstorm', color1: '#314F71', color2: '#4A4176'},
-        {weather: 'Tornado', color1: '#939393', color2: '#e47977c5'}
-    ]
 
-    colorList.forEach(color => {
-        if(color.weather == weather.list[0].weather[0].main) {
-            body.style.backgroundImage = `linear-gradient(to bottom right, ${color.color1}, ${color.color2})`;
+    const colorsByCondition = {
+        Clear: {
+            color1: '#7AE7C7',
+            color2: '#72C1E1'
+        },
+        Clear5: {
+            color1: '#89a1dd',
+            color2: '#E4E5E7'
+          },
+        Clear15: {
+            color1: '#C94926',
+            color2: '#BB9F34'
+          },
+        Clouds: {
+            color1: '#F981BB',
+            color2: '#7F7E84'
+          },
+        Drizzle: {
+            color1: '#b2c9c8', 
+            color2: '#698b8b'
+        },
+        Fog: { 
+            color1: '#C5B2A6', 
+            color2: '#7F7E84'
+        },
+        Rain: { 
+            color1: '#504AC4', 
+            color2: '#59AED1'
+        },
+        Snow: { 
+            color1: '#bfc9cf', 
+            color2: '#77BDE0'
+        },
+        Thunderstorm: { 
+            color1: '#314F71', 
+            color2: '#4A4176'
+        },
+        Tornado: {
+             color1: '#939393', 
+             color2: '#e47977c5'
         }
-    });
+    }
+
+    //Find correct colours
+    const conditions = weather.list[0].weather[0].main;
+    const {color1, color2} = weatherTemp < 5 && conditions == 'Clear' ? colorsByCondition.Clear5 : weatherTemp > 15 && conditions == 'Clear' ? colorsByCondition.Clear15 : colorsByCondition[conditions];
+
+    body.style.backgroundImage = `linear-gradient(to bottom right, ${color1}, ${color2})`;
 }
 
 function dateBuilder(d) {
