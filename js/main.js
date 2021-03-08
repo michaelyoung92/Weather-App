403ab1cfd9ed039a4625e738522177020bcf8bef
Michael Young <michaelbyoung9208@gmail.com>
2021-03-08 20:44:59 +0000

    layout complete, main javascript completed

---
+++ js/main.js
@@ -0,0 +1,103 @@
+const api = {
+    key: "39b44eaa66c8464bda8ff71fc2a80638",
+    link: "https://api.openweathermap.org/data/2.5/"
+}
+
+const submit = document.querySelector('.submit');
+const search = document.querySelector('.search-box');
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    getData(search.value);
+});
+
+//Fetch Weather Data
+function getData(city) {
+    fetch(`${api.link}forecast?q=${city}&units=metric&appid=${api.key}`)
+        .then(weatherData => {
+            return weatherData.json();
+        }).then(displayData);
+}
+
+//Display Weather Data
+function displayData(weather) {
+    console.log(weather);
+
+    //Set Todays Date
+    let date = document.querySelector('.date');
+    let today = new Date();
+    date.innerText = todaysDate(today);
+    
+
+    //Set Weather Icon
+    let weatherIcon = document.querySelector('.weather-icon img');
+    let weatherType = weather.list[0].weather[0].main;
+    const weatherIconsList = [
+        'Clear',
+        'Clouds',
+        'Drizzle',
+        'Fog',
+        'Rain',
+        'Snow',
+        'Thunderstorm',
+        'Tornado'
+    ]
+
+    weatherIconsList.forEach(icon => {
+        if (icon == weatherType) {
+            weatherIcon.src = `img/svg/${icon}.svg`;
+        }
+    });
+
+    //Set Current Temerature
+    let currentTemp = document.querySelector('.current-temp');
+    let weatherTemp = Math.round(weather.list[0].main.temp);
+    currentTemp.innerText = weatherTemp + "°c";
+
+    //Set Weather Description
+    let weatherDescription = document.querySelector('.weather-description');
+    weatherDescription.innerText = weatherType;
+
+    //Set minimum & Maximum Temperatures
+    let minMax = document.querySelector('.min-max');
+    let weatherMin = Math.round(weather.list[0].main.temp_min);
+    let weatherMax = Math.round(weather.list[0].main.temp_max);
+    minMax.innerText = weatherMin + "°c / " + weatherMax + "°c";
+
+    //Set City Name
+    let cityName = document.querySelector('.city');
+    let weatherCityName = weather.city.name;
+    cityName.innerText = weatherCityName;
+
+    //Set Wind Speed
+    let windSpeed = document.querySelector('.wind-speed span');
+    let weatherWindSpeed = Math.round(weather.list[0].wind.speed * 2.23);
+    windSpeed.innerText = weatherWindSpeed + "mph";
+
+    //Set Humidity
+    let humidity = document.querySelector('.humidity span');
+    let weatherHumidity = weather.list[0].main.humidity;
+    humidity.innerText = weatherHumidity + "%";
+
+    //Set Feels Like Temperature
+    let feelsLike = document.querySelector('.feels-like span');
+    let weatherFeelsLike = Math.round(weather.list[0].main.feels_like);
+    feelsLike.innerText = weatherFeelsLike + "°c";
+}
+
+//Return Todays Date
+function todaysDate(d) {
+    const months = [
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ];
+    const days = [
+        "Sun","Mon","Tues","Wed","thurs","Fri","Sat"
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
